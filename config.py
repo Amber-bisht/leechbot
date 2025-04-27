014f3fc99e14e93653be40ddf6046504b127ccc2
Amber-bisht <166226071+Amber-bisht@users.noreply.github.com>
2025-04-28 00:11:37 +0530

    n
    b

---
+++ config.py
@@ -0,0 +1,5 @@
+api_id = "22424291"
+api_hash = "0689d1da53ab769f0381cc5ba6afe6a2"
+bot_token = "7382274538:AAFG7m5S-FHx3FxOZwh_KRBehlgQQzVJ3j4"
+auth_users = [6390375572,2067350865]
+sudo_users = [6390375572,2067350865]
